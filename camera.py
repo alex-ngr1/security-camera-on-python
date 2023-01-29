3411f24c941bfa5b069d96a40aafceca0abcf343
alex-ngr1 <sashafo01928@gmail.com>
2023-01-29 12:51:52 +0200

    my two commit

--- camera.py
+++ camera.py
@@ -10,15 +10,50 @@ face_cascade = cv2.CascadeClassifier(
 body_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_fullbody.xml") 
 
+detection = False
+detection_stopped_time =None
+timer_started = False
+SECPNDS_TO_RECORD_AFTER_DETECTION = 5
+
+frame_size =  (int(cap.get(3)), int(cap.get(4)))
+fourcc = cv2.VideoWriter.fourcc(*"mp4v")
+
 
 while True:
     _, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    bodies = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    for (x, y, width, height) in faces:
-        cv2.rectangle(frame, (x,y),(x + width, y + height), (255, 0, 00), 3)
+    if len(faces) + len(bodies) > 0:
+        if detection:
+            timer_started = False
+        else:
+            detection = True
+            currrent_time = datetime.datetime.now().starttime("%d-%m-%Y-%H-M-%S")
+            out = cv2.VideoWriter(f"{currrent_time}video.mp4", fourcc, 20.0, frame_size)
+            print("Started recording!")
+    elif detection:
+        if timer_started:
+            if time.time() - detection_stopped_time >+ SECPNDS_TO_RECORD_AFTER_DETECTION:
+                detection = False
+                timer_started = False
+                out.release()
+                print('Stop Recording!')
+        else:
+            timer_started = True
+            detection_stopped_time = time.time()
+
+
+
+
+
+    if detection:
+        out.write(frame)
+
+    # for (x, y, width, height) in faces:
+    #    cv2.rectangle(frame, (x,y),(x + width, y + height), (255, 0, 00), 3)
         
 
     cv2.imshow("Camera", frame)
@@ -26,5 +61,7 @@ while True:
     if cv2.waitKey(1) == ord('q'):
         break
 
+
+out.relase()
 cap.release()
 cv2.destroyAllWindows()
